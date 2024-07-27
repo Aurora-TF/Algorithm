8991a4c1d001f042dacc2eae9ffcfbf2d80dd12d
gyu-young-park <gyoue200125@gmail.com>
2024-07-28 03:02:08 +0900

    feat: add solution 1, 2 of week5

---
+++ week5 - set/gyu/3. 영어 끝말잇기.py
@@ -0,0 +1,28 @@
+
+def solution(n, words):
+    dictionary = {}
+    answer = [0, 0]
+    prev = None
+    index = 0
+    turn = 0
+    for i in range(len(words)):
+        index = (i % n) + 1
+        turn = i // n + 1
+        
+        word = words[i] 
+        
+        if word not in dictionary:
+            dictionary[word] = True
+        else:
+          answer = [index, turn]
+          break
+        
+        if prev is None:
+            prev = word
+        elif prev[-1] != word[0]:
+            answer = [index, turn]
+            break
+        
+        prev = word
+    
+    return answer
